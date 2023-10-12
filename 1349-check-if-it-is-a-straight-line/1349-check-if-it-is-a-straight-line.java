a6d95d92541698c46184766383d14b5c8d903a24
Amna Kamran <101377410+jurazil@users.noreply.github.com>
2023-10-12 15:12:41 +0500

    Time: 0 ms (100%), Space: 42.8 MB (72.49%) - LeetHub

--- 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.java
+++ 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean checkStraightLine(int[][] coordinates) {
-        if(coordinates.length<=2) return true;
-        int x0= coordinates[0][0];
-        int y0= coordinates[0][1];
-        int x1=coordinates[1][0];
-        int y1= coordinates[1][1];
-        // double slope=(double) (y1-y0)/(x1-x0);
-        for(int i=2;i<coordinates.length;i++){
-            int x=coordinates[i][0];
-            int y= coordinates[i][1];
-            // double varslope=(double)(y-y0)/(x-x0);
-            // if(slope!=varslope || ) return false; 
-
-            if((y1-y0)*(x-x0)!= (y-y0)*(x1-x0)) return false;
+        if (coordinates.length <= 2) {
+            return true;
         }
-    return true;
+
+        int delX = coordinates[1][0] - coordinates[0][0];
+        int delY = coordinates[1][1] - coordinates[0][1];
+
+        for (int i = 1; i < coordinates.length - 1; i++) {
+            int newDelX = coordinates[i + 1][0] - coordinates[i][0];
+            int newDelY = coordinates[i + 1][1] - coordinates[i][1];
+
+            if (delX * newDelY != delY * newDelX) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
